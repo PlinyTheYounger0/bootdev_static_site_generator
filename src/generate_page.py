e776c07cf35af6eb9aeded5b988b2a2a1605f6ed
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-01-16 11:33:22 -0800

    Maybe it wasn't the entire project in one go

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
             return clean_title
     raise Exception('No title found. Make sure you have an h1 in your markdown.')
 
-def generate_page(src_path, template_path, dest_path):
+def generate_page(src_path, template_path, dest_path, base_path):
     print(f'Generating page from {src_path} to {dest_path} using {template_path}')
 
     with open(src_path, 'r', encoding='utf8') as f:
@@ -27,6 +27,8 @@ def generate_page(src_path, template_path, dest_path):
 
     title_no_content_html = html_template.replace('{{ Title }}', title)
     title_content_html = title_no_content_html.replace('{{ Content }}', html_content)
+    title_content_html = title_content_html.replace('href="/', f'href="{base_path}')
+    title_content_html = title_content_html.replace('src="/', f'src="{base_path}')
 
     dest_dirs_path = os.path.dirname(dest_path)
     os.makedirs(dest_dirs_path, exist_ok=True)
@@ -34,7 +36,7 @@ def generate_page(src_path, template_path, dest_path):
     with open(dest_path, 'w', encoding='utf8') as f:
         f.write(title_content_html)
 
-def generate_page_recursive(src_dir, template_path, dest_dir):
+def generate_page_recursive(src_dir, template_path, dest_dir, base_path):
     files = os.listdir(src_dir)
     print(files)
     for file in files:
@@ -44,7 +46,7 @@ def generate_page_recursive(src_dir, template_path, dest_dir):
              if file.endswith('md'):
                  file = file.replace('md', 'html')
                  dest_path = os.path.join(dest_dir, file)
-             generate_page(src_path, template_path, dest_path)
+             generate_page(src_path, template_path, dest_path, base_path)
         if os.path.isdir(src_path):
-            generate_page_recursive(src_path, template_path, dest_path)
+            generate_page_recursive(src_path, template_path, dest_path, base_path)
         
--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import os
 import shutil
+import sys
 
 from copy_static import copy_files_recursive
 from generate_page import generate_page_recursive
 
 
 dir_path_static = './static'
-dir_path_public = './public'
+dir_path_public = './docs'
 dir_path_content = './content'
 template_path = './template.html'
-
+defalut_base_path = '/'
 
 def main():
+    base_path = defalut_base_path
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+
     print(f' Deleting public directory')
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -22,7 +27,8 @@ def main():
     generate_page_recursive(
         dir_path_content,
         './template.html', 
-        dir_path_public
+        dir_path_public,
+        base_path
     )
 
 if __name__ == '__main__':
